5182b1e0498983b98ba396ec28194701da6d35f9
不說 <49492695+jtlai0921@users.noreply.github.com>
2021-03-02 21:07:03 +0800

    1

--- sample/src/main/java/controller/scoreAction.java
+++ sample/src/main/java/controller/scoreAction.java
@@ -3,5 +3,13 @@ package controller;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class scoreAction extends ActionSupport {
+	private String score;
 
+	public String getScore() {// response
+		return score;
+	}
+
+	public void setScore(String score) {// request
+		this.score = score;
+	}
 }
