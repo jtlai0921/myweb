743e55f6de8d55eb412c2718b5712d4d608c5ea1
不說 <49492695+jtlai0921@users.noreply.github.com>
2021-03-04 20:44:09 +0800

    1

--- hiEx/src/main/java/Dao/studentDao.java
+++ hiEx/src/main/java/Dao/studentDao.java
@@ -2,20 +2,27 @@ package Dao;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+
+import Model.student;
 
 public class studentDao implements implDao{
 
 	public static void main(String[] args) {
-	
+		student s=new student("abc",10,20);
+		new studentDao().add(s);
 
 	}
 
 	@Override
 	public void add(Object o) {
-		// TODO Auto-generated method stub
+		Session se=implDao.getDB();
+		Transaction t=se.beginTransaction();
+		se.save(o);
+		t.commit();
+		se.close();
 		
 	}
-
 
 }
