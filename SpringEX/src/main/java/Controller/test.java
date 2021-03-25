9b127f1e8810a8d3e625480307184e645a7b0d63
不說 <49492695+jtlai0921@users.noreply.github.com>
2021-03-25 20:03:11 +0800

    1

--- SpringEX/src/main/java/Controller/test.java
+++ SpringEX/src/main/java/Controller/test.java
@@ -12,6 +12,19 @@ public class test {
 		System.out.println(f.getA1().getLcd());
 		System.out.println(f.getA2().getLcd());
 		System.out.println(f.getB().getRam());
+		
+		System.out.println("=============================");
+		A a1=new A(30);
+		a1.setLcd(50);
+		A a2=new A(5);
+		B b=new B(15);
+		
+		
+		F1 f2=new F1(a1,a2,b);
+		System.out.println(f2.getA1().getLcd());
+		System.out.println(f2.getA2().getLcd());
+		System.out.println(f2.getB().getRam());
+		
 
 	}
 
