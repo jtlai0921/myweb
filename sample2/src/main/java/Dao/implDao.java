2f87b3ba7edadce34afbf4aee277c3fd0d28e0dc
不說 <49492695+jtlai0921@users.noreply.github.com>
2021-03-30 21:34:59 +0800

    1

--- sample2/src/main/java/Dao/implDao.java
+++ sample2/src/main/java/Dao/implDao.java
@@ -1,5 +1,27 @@
 package Dao;
 
-public class implDao {
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+public interface implDao {
+//連線
+		static Session getDB()
+		{
+			Configuration con=new Configuration().configure();
+			SessionFactory sf=con.buildSessionFactory();
+			Session se=sf.openSession();
+			
+			return se;
+		}
+		
+	static ApplicationContext a1=new ClassPathXmlApplicationContext("sp1.xml");	
+		
+	
+	//新增
+		
+		void add(Object o);
+	
 }
--- sample2/src/main/java/Dao/memberDao.java
+++ sample2/src/main/java/Dao/memberDao.java
@@ -1,5 +1,22 @@
 package Dao;
 
-public class memberDao {
+import com.mysql.fabric.xmlrpc.base.Member;
+
+import Model.member;
+
+public class memberDao implements implDao{
+
+	public static void main(String[] args) {
+	member m=implDao.a1.getBean("m",member.class);
+	
+	System.out.println(m.getName());
+	
+	}
+
+	@Override
+	public void add(Object o) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
