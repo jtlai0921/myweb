79726691c9a703119a1739be2b6696f086e0ab06
不說 <49492695+jtlai0921@users.noreply.github.com>
2021-04-06 18:49:56 +0800

    1

--- sample2/src/main/java/Dao/memberDao.java
+++ sample2/src/main/java/Dao/memberDao.java
@@ -1,7 +1,10 @@
 package Dao;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 import com.mysql.fabric.xmlrpc.base.Member;
 
@@ -10,12 +13,18 @@ import Model.member;
 public class memberDao implements implDao{
 
 	public static void main(String[] args) {
-	member m=implDao.a1.getBean("m",member.class);
 	
-	//System.out.println(m.getName());
-	
-	new memberDao().add(m);
-	
+		List l=new memberDao().queryAll();
+		member[] m=(member[]) l.toArray(new member[l.size()]);
+		String sum="";
+		
+		for(String o:m[1].getInterest())
+		{
+			sum=sum+o+" ";
+		}
+		
+		
+		System.out.println(m[1].getName()+"\t"+m[1].getAddress()+"\t"+sum);
 	}
 
 	@Override
@@ -26,6 +35,16 @@ public class memberDao implements implDao{
 		t.commit();
 		se.close();
 		
+	}
+
+	public List queryAll() {
+		Session se=implDao.getDB();
+		String HQL="from member";
+		Query q=se.createQuery(HQL);
+		
+		List l=q.list();
+		
+		return l;
 	}
 
 }
