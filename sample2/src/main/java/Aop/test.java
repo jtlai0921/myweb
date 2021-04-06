07e914b11e18c2cc7458ad7929cab8d107a39f76
不說 <49492695+jtlai0921@users.noreply.github.com>
2021-04-06 20:11:30 +0800

    1

---
+++ sample2/src/main/java/Aop/test.java
@@ -0,0 +1,18 @@
+package Aop;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import Dao.implDao;
+
+public class test {
+
+	public static void main(String[] args) {
+		ApplicationContext a1=new ClassPathXmlApplicationContext("sp3.xml");
+		implAop iaop=(implAop) a1.getBean("aopDao");
+		iaop.withAop();
+//		
+//		new AopDao().withAop();
+	}
+
+}
